05d3f5caca6047955fcafa8ab01c0c4c56890b5e
Naveen Edala <44523972+NaveenEdala@users.noreply.github.com>
2019-09-11 11:44:56 +0530

    Create equalSidesArray.py

---
+++ Python/equalSidesArray.py
@@ -0,0 +1,7 @@
+# https://www.codewars.com/kata/5679aa472b8f57fb8c000047/
+
+def find_even_index(arr):
+    for i in range(len(arr)):
+        if sum(arr[:i]) == sum(arr[i+1:]):
+            return i
+    return -1
