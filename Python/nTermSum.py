9f5a2f5a52895a5019892c0563018ac035f251d7
Naveen Edala <44523972+NaveenEdala@users.noreply.github.com>
2019-09-11 10:35:56 +0530

    Create nTermSum.py

---
+++ Python/nTermSum.py
@@ -0,0 +1,10 @@
+#https://www.codewars.com/kata/555eded1ad94b00403000071
+
+def series_sum(n):
+    # Happy Coding ^_^
+    x = 1
+    result = 0
+    for i in range(n):
+        result += (1/x)
+        x += 3
+    return '{:.2f}'.format(result)
