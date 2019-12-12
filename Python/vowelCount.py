b7ae3e384cae8c5028e49d9cb0f34f359cdb2e71
Naveen Edala <44523972+NaveenEdala@users.noreply.github.com>
2019-12-12 19:34:36 +0530

    Create vowelCount.py

---
+++ Python/vowelCount.py
@@ -0,0 +1,8 @@
+# https://www.codewars.com/kata/54ff3102c1bad923760001f3
+
+def getCount(inputStr):
+    num_vowels = 0
+    for letter in inputStr:
+        if letter in "aeiou":
+            num_vowels+=1
+    return num_vowels
